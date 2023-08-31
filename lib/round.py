efa4d6d41aec42136d93a01f71f666a2348d244b
Kerynn Davis <kerynn.davis@gmail.com>
2023-08-31 09:30:30 -0700

    fix formatting

--- lib/round.py
+++ lib/round.py
@@ -6,30 +6,30 @@ class Round:
     card = random.choice(cards)
     return card 
 
-def calclulate_score(current_hand):
-  total_score = sum(current_hand)
-  if total_score == 21 and len(current_hand) == 2:
-    total_score = 0
-  if 11 in current_hand and total_score > 21:
-    current_hand.remove(11)
-    current_hand.append(1)
-  return total_score
+  def calclulate_score(current_hand):
+    total_score = sum(current_hand)
+    if total_score == 21 and len(current_hand) == 2:
+      total_score = 0
+    if 11 in current_hand and total_score > 21:
+      current_hand.remove(11)
+      current_hand.append(1)
+    return total_score
 
-def compare(user_score, computer_score):
-  if user_score > 21 and computer_score > 21:
-    return "Your score is over 21. You lose!"
-  elif user_score == computer_score:
-    return "You tied with the computer!"
-  elif computer_score == 0:
-    return "The computer has a blackjack. You lose!"
-  elif user_score == 0:
-    return "You win with a blackjack!"
-  elif user_score > 21:
-    return "Your score is over 21. You lose!"
-  elif computer_score > 21:
-    return "The computer went over 21. You win!"
-  elif user_score > computer_score:
-    return "You win!"
-  else: 
-    return "You lose!"
+  def compare(user_score, computer_score):
+    if user_score > 21 and computer_score > 21:
+      return "Your score is over 21. You lose!"
+    elif user_score == computer_score:
+      return "You tied with the computer!"
+    elif computer_score == 0:
+      return "The computer has a blackjack. You lose!"
+    elif user_score == 0:
+      return "You win with a blackjack!"
+    elif user_score > 21:
+      return "Your score is over 21. You lose!"
+    elif computer_score > 21:
+      return "The computer went over 21. You win!"
+    elif user_score > computer_score:
+      return "You win!"
+    else: 
+      return "You lose!"
 
